ad2c85029c23c7a94b044f6b9d1ebb00b776f952
mokes <mokes@otherMynd.com>
2024-11-22 21:27:09 +0000

    last small fixes

--- components/Sidebar/SearchForm.tsx
+++ components/Sidebar/SearchForm.tsx
@@ -1,30 +1,23 @@
 "use client"
 
 import { Search } from 'lucide-react'
-
 import { Label } from '@/components/ui/label'
-import {
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarInput,
-} from '@/components/ui/sidebar'
+import { Input } from '@/components/ui/input'
 
 export function SearchForm({ ...props }: React.ComponentProps<'form'>) {
   return (
     <form {...props}>
-      <SidebarGroup className="py-0">
-        <SidebarGroupContent className="relative">
-          <Label htmlFor="search" className="sr-only">
-            Search
-          </Label>
-          <SidebarInput
-            id="search"
-            placeholder="Search canvases..."
-            className="pl-8"
-          />
-          <Search className="pointer-events-none absolute left-2 top-1/2 size-4 -translate-y-1/2 select-none opacity-50" />
-        </SidebarGroupContent>
-      </SidebarGroup>
+      <div className="relative">
+        <Label htmlFor="search" className="sr-only">
+          Search
+        </Label>
+        <Input
+          id="search"
+          placeholder="Search canvases..."
+          className="pl-8"
+        />
+        <Search className="pointer-events-none absolute left-2 top-1/2 size-4 -translate-y-1/2 select-none opacity-50" />
+      </div>
     </form>
   )
 }
