daea83f8bb76fd45fcf9f8c8459a3f9c78c87ca7
mokes <mokes@otherMynd.com>
2024-11-26 19:10:15 +0000

    fix build

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,14 +1,22 @@
 // firebaseAdmin.ts
 import * as admin from 'firebase-admin';
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-    }),
-  });
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || '{}')
+if (!admin.apps.length) {   
+    //if serviceAccount is not empty, use it to initialize the app
+    if (Object.keys(serviceAccount).length > 0) {
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount),
+        });
+        } else {
+        admin.initializeApp({
+            credential: admin.credential.cert({
+                privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+                clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+                projectId: process.env.FIREBASE_PROJECT_ID,
+            }),
+        });
+    }
 }
 
 const db = admin.firestore();
