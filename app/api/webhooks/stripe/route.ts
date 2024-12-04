27074c559b9ebc35d6edd582bd13edb7abe958d7
mokes <mokes@otherMynd.com>
2024-12-04 13:00:02 +0000

    fix build

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -5,7 +5,7 @@ import { db } from '@/lib/firebase';
 import { doc, setDoc, getDoc } from 'firebase/firestore';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-10-28.acacia',
+  apiVersion: '2024-11-20.acacia',
 })
 
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!
